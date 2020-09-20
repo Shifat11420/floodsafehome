aa2bafede38590a2c89e47f10e846c918b51c0b7
Shifat11420 <shifat.perveen@gmail.com>
2020-09-19 23:23:30 -0500

    list view of database freeboardconstuctioncost

--- wwwroot/rootProject/rootApp/admin.py
+++ wwwroot/rootProject/rootApp/admin.py
@@ -3,6 +3,9 @@ from rootApp.models import Contact
 from rootApp.models import FreeboardConstructionCost
 
 
+class FreeboardConstructionCostAdmin(admin.ModelAdmin):
+    list_display = ("address", "street", "floodzone", "parish", "no_floors")
+
 # Register your models here.
 admin.site.register(Contact)
-admin.site.register(FreeboardConstructionCost)
+admin.site.register(FreeboardConstructionCost, FreeboardConstructionCostAdmin)
