b227810cbbc08ab6b9751a476647bac26bda5fc8
Shifat <smithi1@lsu.edu>
2020-09-21 02:54:03 -0500

    listview of construction cost model

--- wwwroot/rootProject/rootApp/admin.py
+++ wwwroot/rootProject/rootApp/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from rootApp.models import Contact
 from rootApp.models import FreeboardConstructionCost
 
+class FreeboardConstructionCostAdmin(admin.ModelAdmin):
+    list_display = ("address", "street", "floodzone", "parish", "no_floors",)
 
 # Register your models here.
 admin.site.register(Contact)
-admin.site.register(FreeboardConstructionCost)
+admin.site.register(FreeboardConstructionCost, FreeboardConstructionCostAdmin)
