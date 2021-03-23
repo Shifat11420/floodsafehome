91dbfa2c18ad6c152bf88cafbdb28a922106946e
Jiyoung <jiyoungis0915@gmail.com>
2021-03-23 17:58:37 -0500

    Add fields

--- wwwroot/rootProject/static/js/disclaimer.js
+++ wwwroot/rootProject/static/js/disclaimer.js
@@ -213,3 +213,23 @@ function ShowHideDiv2() {
   SpatialScale.style.display = chkNo.checked ? "block" : "none";
   
 }
+
+
+/* Add/Delete fields */
+function add(type) {
+
+	//Create an input type dynamically.
+	var element = document.createElement("input");
+
+	//Assign different attributes to the element.
+	element.setAttribute("type", type);
+	element.setAttribute("value", type);
+	element.setAttribute("name", type);
+
+
+	var foo = document.getElementById("fooBar");
+
+	//Append the element in page (in span).
+	foo.appendChild(element);
+
+}
