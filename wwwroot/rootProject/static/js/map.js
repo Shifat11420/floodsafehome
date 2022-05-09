6633c04929574a3cc96a835ccfdf42812dc95f8a
Jiyoung Lee <jiyoungis0915@gmail.com>
2022-05-09 13:30:55 -0500

    Fix errors (Img and slider)

--- wwwroot/rootProject/static/js/map.js
+++ wwwroot/rootProject/static/js/map.js
@@ -75,23 +75,24 @@ jQuery(document).ready(function($){
   // Hide Header on on scroll down 
   
   /*image slider*/
-  var img = document.getElementById('img');
+  var img = document.getElementById("img");
+  var img2 = document.getElementById("img2");
+  var img3 = document.getElementById("img3");
+  var img4 = document.getElementById("img4");
+
   var img_array = ['static/img/construction/zero.png','static/img/construction/onefoot.png','static/img/construction/twofeet.png', 'static/img/construction/threefeet.png', 'static/img/construction/fourfeet.png'];
+  
   function setImage(obj)
   {
   var value = obj.value;
   img.src = img_array[value];
+  img2.src = img_array[value];
+  img3.src = img_array[value];
+  img4.src = img_array[value];
     
   }
 
-  var img2 = document.getElementById('img2');
-  var img2_array = ['static/img/construction/zero.png','static/img/construction/onefoot.png','static/img/construction/twofeet.png', 'static/img/construction/threefeet.png', 'static/img/construction/fourfeet.png'];
-  function setImage(obj)
-  {
-  var value = obj.value;
-  img2.src = img2_array[value];
-    
-  }
+  
   
   /* slider number:X zone with insurance*/
   var slider = document.getElementById("myRange");
@@ -200,6 +201,329 @@ jQuery(document).ready(function($){
   }
   })();
 
+  /* slider number:X zone without insurance*/
+  var slider2 = document.getElementById("myRangeXzoneWithout");
+  var output2 = document.getElementById("demo_Xzone_without");
+  output2.innerHTML = slider2.value;
+  
+  slider2.oninput = function() {
+  output2.innerHTML = this.value;
+  }
+  
+  
+  var freeboard2 = document.getElementById("freeboard");
+  var freeboard_array2 = ["{{FreeboardCost0}}", "{{FreeboardCost1}}", "{{FreeboardCost2}}", "{{FreeboardCost3}}", "{{FreeboardCost4}}"];
+  function setValue()
+  {
+  var value2 = obj.value;
+  freeboard2.scr = freeboard_array2[value];
+  }
+  
+  (function() {
+  const pricingSliders = document.querySelectorAll(".pricing-slider");
+  
+  if (pricingSliders.length > 0) {
+    for (let i = 0; i < pricingSliders.length; i++) {
+      const pricingSlider = pricingSliders[i];
+  
+      // Build the input object
+      const pricingInput = {
+        el: pricingSlider.querySelector("input")
+      };
+      pricingInput.data = JSON.parse(
+        pricingInput.el.getAttribute("data-price-input")
+      );
+      pricingInput.currentValEl = pricingSlider.querySelector(
+        ".pricing-slider-value"
+      );
+      pricingInput.thumbSize = parseInt(
+        window
+          .getComputedStyle(pricingInput.currentValEl)
+          .getPropertyValue("--thumb-size"),
+        10
+      );
+  
+      // Build the output array
+      const pricingOutputEls = pricingSlider.parentNode.querySelectorAll(
+        ".pricing-item-price"
+      );
+      const pricingOutput = [];
+      for (let i = 0; i < pricingOutputEls.length; i++) {
+        const pricingOutputEl = pricingOutputEls[i];
+        const pricingOutputObj = {};
+        pricingOutputObj.currency = pricingOutputEl.querySelector(
+          ".pricing-item-price-currency"
+        );
+        pricingOutputObj.amount = pricingOutputEl.querySelector(
+          ".pricing-item-price-amount"
+        );
+        pricingOutputObj.after = pricingOutputEl.querySelector(
+          ".pricing-item-price-after"
+        );
+        pricingOutputObj.data = JSON.parse(
+          pricingOutputEl.getAttribute("data-price-output")
+        );
+        pricingOutput.push(pricingOutputObj);
+      }
+        
+      pricingInput.el.setAttribute("min", 0);
+      pricingInput.el.setAttribute(
+        "max",
+        Object.keys(pricingInput.data).length - 1
+      );
+      !pricingInput.el.getAttribute("value") &&
+        pricingInput.el.setAttribute("value", 0);
+  
+      handlePricingSlider(pricingInput, pricingOutput);
+      window.addEventListener("input", function() {
+        handlePricingSlider(pricingInput, pricingOutput);
+      });
+    }
+  }
+  
+  function handlePricingSlider(input, output2) {
+    // output the current slider value
+    if (input.currentValEl)
+      input.currentValEl.innerHTML = input.data[input.el.value];
+    // update prices
+    for (let i = 0; i < output2.length; i++) {
+      const outputObj = output2[i];
+      if (outputObj.currency)
+        outputObj.currency.innerHTML = outputObj.data[input.el.value][0];
+      if (outputObj.amount)
+        outputObj.amount.innerHTML = outputObj.data[input.el.value][1];
+      if (outputObj.after)
+        outputObj.after.innerHTML = outputObj.data[input.el.value][2];
+    }
+    handleSliderValuePosition(input);
+  }
+  
+  function handleSliderValuePosition(input) {
+    const multiplier = input.el.value / input.el.max;
+    const thumbOffset = input.thumbSize * multiplier;
+    const priceInputOffset =
+      (input.thumbSize - input.currentValEl.clientWidth) / 2;
+    input.currentValEl.style.left =
+      input.el.clientWidth * multiplier - thumbOffset + priceInputOffset + "px";
+  }
+  })();
+
+
+
+ /* slider number:Other zone with insurance*/
+ var slider3 = document.getElementById("myRange_OtherZone_with");
+ var output3 = document.getElementById("demo_OtherZone_with");
+ output3.innerHTML = slider3.value;
+ 
+ slider3.oninput = function() {
+ output3.innerHTML = this.value;
+ }
+ 
+ 
+ var freeboard3 = document.getElementById("freeboard");
+ var freeboard_array3 = ["{{FreeboardCost0}}", "{{FreeboardCost1}}", "{{FreeboardCost2}}", "{{FreeboardCost3}}", "{{FreeboardCost4}}"];
+ function setValue()
+ {
+ var value3 = obj.value;
+ freeboard3.scr = freeboard_array3[value];
+ }
+ 
+ (function() {
+ const pricingSliders = document.querySelectorAll(".pricing-slider");
+ 
+ if (pricingSliders.length > 0) {
+   for (let i = 0; i < pricingSliders.length; i++) {
+     const pricingSlider = pricingSliders[i];
+ 
+     // Build the input object
+     const pricingInput = {
+       el: pricingSlider.querySelector("input")
+     };
+     pricingInput.data = JSON.parse(
+       pricingInput.el.getAttribute("data-price-input")
+     );
+     pricingInput.currentValEl = pricingSlider.querySelector(
+       ".pricing-slider-value"
+     );
+     pricingInput.thumbSize = parseInt(
+       window
+         .getComputedStyle(pricingInput.currentValEl)
+         .getPropertyValue("--thumb-size"),
+       10
+     );
+ 
+     // Build the output array
+     const pricingOutputEls = pricingSlider.parentNode.querySelectorAll(
+       ".pricing-item-price"
+     );
+     const pricingOutput = [];
+     for (let i = 0; i < pricingOutputEls.length; i++) {
+       const pricingOutputEl = pricingOutputEls[i];
+       const pricingOutputObj = {};
+       pricingOutputObj.currency = pricingOutputEl.querySelector(
+         ".pricing-item-price-currency"
+       );
+       pricingOutputObj.amount = pricingOutputEl.querySelector(
+         ".pricing-item-price-amount"
+       );
+       pricingOutputObj.after = pricingOutputEl.querySelector(
+         ".pricing-item-price-after"
+       );
+       pricingOutputObj.data = JSON.parse(
+         pricingOutputEl.getAttribute("data-price-output")
+       );
+       pricingOutput.push(pricingOutputObj);
+     }
+       
+     pricingInput.el.setAttribute("min", 0);
+     pricingInput.el.setAttribute(
+       "max",
+       Object.keys(pricingInput.data).length - 1
+     );
+     !pricingInput.el.getAttribute("value") &&
+       pricingInput.el.setAttribute("value", 0);
+ 
+     handlePricingSlider(pricingInput, pricingOutput);
+     window.addEventListener("input", function() {
+       handlePricingSlider(pricingInput, pricingOutput);
+     });
+   }
+ }
+ 
+ function handlePricingSlider(input, output3) {
+   // output the current slider value
+   if (input.currentValEl)
+     input.currentValEl.innerHTML = input.data[input.el.value];
+   // update prices
+   for (let i = 0; i < output3.length; i++) {
+     const outputObj = output3[i];
+     if (outputObj.currency)
+       outputObj.currency.innerHTML = outputObj.data[input.el.value][0];
+     if (outputObj.amount)
+       outputObj.amount.innerHTML = outputObj.data[input.el.value][1];
+     if (outputObj.after)
+       outputObj.after.innerHTML = outputObj.data[input.el.value][2];
+   }
+   handleSliderValuePosition(input);
+ }
+ 
+ function handleSliderValuePosition(input) {
+   const multiplier = input.el.value / input.el.max;
+   const thumbOffset = input.thumbSize * multiplier;
+   const priceInputOffset =
+     (input.thumbSize - input.currentValEl.clientWidth) / 2;
+   input.currentValEl.style.left =
+     input.el.clientWidth * multiplier - thumbOffset + priceInputOffset + "px";
+ }
+ })();
+
+
+  /* slider number:Other zone with insurance*/
+  var slider4 = document.getElementById("myRange_OtherZone_without");
+  var output4 = document.getElementById("demo_OtherZone_without");
+  output4.innerHTML = slider4.value;
+  
+  slider4.oninput = function() {
+  output4.innerHTML = this.value;
+  }
+  
+  
+  var freeboard4 = document.getElementById("freeboard");
+  var freeboard_array4 = ["{{FreeboardCost0}}", "{{FreeboardCost1}}", "{{FreeboardCost2}}", "{{FreeboardCost3}}", "{{FreeboardCost4}}"];
+  function setValue()
+  {
+  var value4 = obj.value;
+  freeboard4.scr = freeboard_array4[value];
+  }
+  
+  (function() {
+  const pricingSliders = document.querySelectorAll(".pricing-slider");
+  
+  if (pricingSliders.length > 0) {
+    for (let i = 0; i < pricingSliders.length; i++) {
+      const pricingSlider = pricingSliders[i];
+  
+      // Build the input object
+      const pricingInput = {
+        el: pricingSlider.querySelector("input")
+      };
+      pricingInput.data = JSON.parse(
+        pricingInput.el.getAttribute("data-price-input")
+      );
+      pricingInput.currentValEl = pricingSlider.querySelector(
+        ".pricing-slider-value"
+      );
+      pricingInput.thumbSize = parseInt(
+        window
+          .getComputedStyle(pricingInput.currentValEl)
+          .getPropertyValue("--thumb-size"),
+        10
+      );
+  
+      // Build the output array
+      const pricingOutputEls = pricingSlider.parentNode.querySelectorAll(
+        ".pricing-item-price"
+      );
+      const pricingOutput = [];
+      for (let i = 0; i < pricingOutputEls.length; i++) {
+        const pricingOutputEl = pricingOutputEls[i];
+        const pricingOutputObj = {};
+        pricingOutputObj.currency = pricingOutputEl.querySelector(
+          ".pricing-item-price-currency"
+        );
+        pricingOutputObj.amount = pricingOutputEl.querySelector(
+          ".pricing-item-price-amount"
+        );
+        pricingOutputObj.after = pricingOutputEl.querySelector(
+          ".pricing-item-price-after"
+        );
+        pricingOutputObj.data = JSON.parse(
+          pricingOutputEl.getAttribute("data-price-output")
+        );
+        pricingOutput.push(pricingOutputObj);
+      }
+        
+      pricingInput.el.setAttribute("min", 0);
+      pricingInput.el.setAttribute(
+        "max",
+        Object.keys(pricingInput.data).length - 1
+      );
+      !pricingInput.el.getAttribute("value") &&
+        pricingInput.el.setAttribute("value", 0);
+  
+      handlePricingSlider(pricingInput, pricingOutput);
+      window.addEventListener("input", function() {
+        handlePricingSlider(pricingInput, pricingOutput);
+      });
+    }
+  }
+  
+  function handlePricingSlider(input, output4) {
+    // output the current slider value
+    if (input.currentValEl)
+      input.currentValEl.innerHTML = input.data[input.el.value];
+    // update prices
+    for (let i = 0; i < output4.length; i++) {
+      const outputObj = output4[i];
+      if (outputObj.currency)
+        outputObj.currency.innerHTML = outputObj.data[input.el.value][0];
+      if (outputObj.amount)
+        outputObj.amount.innerHTML = outputObj.data[input.el.value][1];
+      if (outputObj.after)
+        outputObj.after.innerHTML = outputObj.data[input.el.value][2];
+    }
+    handleSliderValuePosition(input);
+  }
+  
+  function handleSliderValuePosition(input) {
+    const multiplier = input.el.value / input.el.max;
+    const thumbOffset = input.thumbSize * multiplier;
+    const priceInputOffset =
+      (input.thumbSize - input.currentValEl.clientWidth) / 2;
+    input.currentValEl.style.left =
+      input.el.clientWidth * multiplier - thumbOffset + priceInputOffset + "px";
+  }
+  })();
 
   
   /*Toggling visibility*/
